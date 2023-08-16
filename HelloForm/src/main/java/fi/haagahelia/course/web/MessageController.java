14d8b72ea54338c4eb55a46dcce1d612528ece0a
Pellikka Minna <h01340@haaga-helia.fi>
2023-08-16 19:51:29 +0300

    upgrade to 3.1.3 and JUnit5

--- HelloForm/src/main/java/fi/haagahelia/course/web/MessageController.java
+++ HelloForm/src/main/java/fi/haagahelia/course/web/MessageController.java
@@ -1,10 +1,7 @@
 package fi.haagahelia.course.web;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
