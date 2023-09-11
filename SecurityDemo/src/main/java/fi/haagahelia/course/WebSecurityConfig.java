9635a52eb4bd9e28b98fabba0afa9fc650bd2101
Bergius Tanja <berta@haaga-helia.fi>
2023-09-11 13:13:13 +0300

    updated to Security 6 (pom.xml, Config with lambda, th namespace...)

--- SecurityDemo/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ SecurityDemo/src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -11,23 +11,29 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig  {
-    protected void configure(HttpSecurity http) throws Exception {
+	
+	@Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+            .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/", "/home").permitAll()
                 .anyRequest().authenticated()
-                .and()
-            .formLogin()
+            )
+            .formLogin(formlogin -> formlogin
                 .loginPage("/login")
                 .defaultSuccessUrl("/home", true)
                 .permitAll()
-                .and()
-            .logout()
-                .permitAll();
+            )
+            .logout(logout -> logout
+                .permitAll()
+            );
+        	return http.build();
+            
     }
 
 
