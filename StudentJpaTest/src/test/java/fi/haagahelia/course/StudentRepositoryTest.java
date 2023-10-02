5f21a94cccf1649f5168268abc0883d1f2c50675
Juslin Jukka <jusju@haaga-helia.fi>
2023-10-02 11:52:24 +0300

    updated imports to spring framework 3.1.2 in test code

--- StudentJpaTest/src/test/java/fi/haagahelia/course/StudentRepositoryTest.java
+++ StudentJpaTest/src/test/java/fi/haagahelia/course/StudentRepositoryTest.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.junit.jupiter.api.Test;
