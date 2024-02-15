cefd065582a2ec6cda8734fec8b0cc8a05c7ea26
h01340 <minna.pellikka@haaga-helia.fi>
2024-02-15 08:18:11 +0200

    Upgraded SB version and removed unused imports

--- StudentListRest/src/main/java/fi/haagahelia/course/domain/Student.java
+++ StudentListRest/src/main/java/fi/haagahelia/course/domain/Student.java
@@ -1,15 +1,11 @@
 package fi.haagahelia.course.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Student {
